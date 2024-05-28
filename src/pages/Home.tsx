81462cfa774824c8a93580dbb0cd99e66d22a72b
criss <mediocero007.12@gmail.com>
2024-05-28 16:55:41 -0500

    se agrega grilla de productos al menu principal

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,7 @@
+import { Col, Row } from "react-bootstrap";
 import DarkVariantExample from "../Components/Slider.tsx";
+import { StoreItem } from "../Components/StoreItem.tsx";
+import storeItems2 from "../data/items2.json";
 
 export default function Home() {
   return (
@@ -10,7 +13,17 @@ export default function Home() {
           height: "600px",
         }}
       ></div>
-      <p className='text-center fs-1 m-2'>¡Bienvenido a nuestra tienda deportiva!</p>
+      <h1 className='text-center my-4'>¡Bienvenido a nuestra tienda deportiva!</h1>
+      <p className='text-center my-2 fs-2'>
+        Nuestros productos son de la mejor calidad y a los mejores precios. ¡No te los pierdas!
+      </p>
+      <Row md={2} xs={1} lg={3} className='g-3'>
+        {storeItems2.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 }
